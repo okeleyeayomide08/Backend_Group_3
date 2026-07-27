272dfb22fbe8651cae4954a3eabae68a79b4c9a9
Okeleye Ayomide <okeleyeayomide08@gmail.com>
2026-07-28 00:32:43 +0100

    Added render link to swagger config

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -31,7 +31,7 @@ const options = {
         description: "Development server",
       },
       {
-        url: `https://your-render-url.onrender.com/api`,
+        url: `https://backend-group-3.onrender.com`,
         description: "Production server",
       },
     ],
